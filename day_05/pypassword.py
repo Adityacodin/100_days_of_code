6085ca57cb72fe193899c30bec3ec4dc4da8ce4b
Adityacodin <mohite.aditya.comp@kgce.edu.in>
2024-09-05 11:56:23 +0530

    minor changes to the day5 proj

--- day_05/pypassword.py
+++ day_05/pypassword.py
@@ -1,6 +1,6 @@
 import random
 
-letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 numbers = ['0','1','2','3','4','5','6','7','8','9']
 symbols = ['!,''@','#','$','%','^','&','*','(',')']
 
@@ -14,16 +14,16 @@ for i in range(0,no_of_letters):
     password.append(random.choice(letters))
 
 for i in range(0,no_of_numbers):
-    ind = random.randint(0,len(password))
-    password.insert(ind,random.choice(numbers))
+    password.append(random.choice(numbers))
     
 for i in range(0,no_of_symbols):
-    ind = random.randint(0,len(password))
-    password.insert(ind,random.choice(symbols))
+    password.append(random.choice(symbols))
 
 password_as_string = ''
+random.shuffle(password)
+
 for characters in password:
     password_as_string += characters
 
-
-print(password_as_string)
+print(password)
+print('Your password is : ' + password_as_string)
