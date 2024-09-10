72fcf370436b6cf57ead85eb176e105ecf5f4b21
Adityacodin <mohite.aditya.comp@kgce.edu.in>
2024-09-10 12:06:58 +0530

    minor changes to the auction program

--- day_09/bid_auction.py
+++ day_09/bid_auction.py
@@ -1,5 +1,18 @@
 from art import logo
 
+print(logo)
+print()
+
+def find_highest_bidder(bidding_list):
+    highest_bid = 0
+    winner = None
+    for key in bidders:
+        if highest_bid < bidders[key]:
+            highest_bid = bidders[key]
+            winner = key
+    print(f"{winner} has won with the bid of {highest_bid}$")
+
+
 bidders = {}
 cont = True
 to_continue = None
@@ -13,13 +26,4 @@ while cont:
     print("\n" * 100)
     if to_continue.lower() == 'no':
         cont = False
-
-maxi = 0
-winner = None
-for key in bidders:
-    if maxi < bidders[key]:
-        maxi = bidders[key]
-        winner = key
-    
-
-print(f"{winner} has won with the bid of {maxi}$")
+        find_highest_bidder(bidders)
