1b31da15f9a01be49a2740c02554d97c84aa0b0e
Adityacodin <mohite.aditya.comp@kgce.edu.in>
2024-10-02 10:55:38 +0530

    rm highscore folder

--- day_20/day_24/update.py
+++ day_20/day_24/update.py
@@ -1,12 +1,12 @@
 
 class HighscoreManager():
     def __init__(self):
-        with open('day_20/day_24/highscore.txt',mode='r') as self.file:
+        with open('C:/Users/33333333333333333333/100_days_ofcode/day_24/highscore.txt',mode='r') as self.file:
             self.old_score = int(self.file.read().split(' ')[-1])
 
     def update_highscore(self,new_score):
         if new_score>self.old_score:
-            with open('day_20/day_24/highscore.txt',mode='w') as self.file:
+            with open('C:/Users/33333333333333333333/100_days_ofcode/day_24/highscore.txt',mode='w') as self.file:
                 self.file.write(f'highscore = {new_score}')
 
     def get_highscore(self):
